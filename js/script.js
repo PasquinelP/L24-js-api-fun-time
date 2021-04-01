205f2666af42c1ecd5aae0872711969b23d7a96a
Pascal Pater <pascal.pater@gmail.com>
2021-04-01 20:43:11 +0200

    add second endpoint to show joke as img and show img in result list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 // get needed elements
 const button = document.querySelector("#button-joke");
+const buttonImg = document.querySelector("#button-img-joke");
 const showJoke = document.querySelector("#joke");
+const showJokeImg = document.querySelector("#joke-img");
 
 // use data from api
 // use the right key-value and show within the html element
@@ -12,3 +14,21 @@ const showDadJoke = async () => {
 };
 
 button.addEventListener("click", showDadJoke);
+
+
+// use data from api to show joke as image
+// get joke id to construct img url
+// create img element and add src URL
+// show within html element
+const showDadJokeAsImage = async () => {
+  const data = await getData();
+  const dadJokeID = data.id;
+  const dadJokeImgUrl = `https://icanhazdadjoke.com/j/${dadJokeID}.png`;
+  const dadJokeImg = document.createElement("img");
+  dadJokeImg.src = dadJokeImgUrl;
+  console.log(dadJokeImg);
+  console.log(dadJokeImgUrl);
+  showJokeImg.appendChild(dadJokeImg);
+};
+
+buttonImg.addEventListener("click", showDadJokeAsImage);
